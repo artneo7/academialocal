e1ea45b1a487bdc78a60bac57dbcdb7f23e9f718
Andre Esteves Perrone <contato@artneo.com.br>
2023-02-13 15:49:14 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import '../global.css'
+  import "@fontsource/roboto-condensed"
+  import "@fontsource/roboto-condensed/700.css"
+  import "@fontsource/cabin"
+</script>
+
+<slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<h2>Teste</h2>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+  h1 {
+    color: var(--feat);
+  }
+</style>
