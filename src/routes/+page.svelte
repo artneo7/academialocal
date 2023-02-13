0d555e16980a77a3e8f56af8479f1cb1ee10ddb3
Andre Esteves Perrone <contato@artneo.com.br>
2023-02-13 16:55:42 -0300

    início da codificação

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<h2>Teste</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import imgHome from '../assets/musculacao.jpg'
+</script>
+
+<section class="intro" style="background-image: url({imgHome})">
+  <div class="intro__wrapper wrap">
+    <h1>Você está pronto para o desafio?</h1>
+    <p>A Academia Local está pronta para te ajudar a chegar mais longe e alcançar sua melhor performance.</p>
+    <a href="/sobre">Sobre</a>
+  </div>
+</section>
 
 <style>
-  h1 {
-    color: var(--feat);
+  .intro {
+    min-height: 360px;
+    background-size: cover;
+    background-position: center;
+    display: grid;
+    align-content: center;
+    justify-content: start;
+    position: relative;
+  }
+  .intro::after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background-color: rgba(0,0,0,0.55);
+  }
+  .intro__wrapper {
+    z-index: 1;
   }
 </style>
