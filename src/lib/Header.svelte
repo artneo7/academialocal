52697a57c5e84b46ed3085a576cd8f2ca7de779e
Andre Esteves Perrone <contato@artneo.com.br>
2023-02-14 10:48:00 -0300

    header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,48 @@
+<script>
+  import logo from '$lib/assets/logo.png';
+</script>
+
+<header class="header">
+  <div class="header__wrapper wrap">
+    <a href="/" class="logo"><img src="{logo}" alt="Logo da Academia Local"></a>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/sobre">Sobre</a></li>
+        <li><a href="/modalidades">Modalidades</a></li>
+        <li><a class="btn header__btn" href="#contato">Contato</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<style>
+  .header__btn {
+    border-color: var(--feat);
+  }
+  .header__btn:hover {
+    border-color: #fff;
+  }
+  .header__wrapper {
+    display: flex;
+    align-items: center;
+    min-height: 120px;
+  }
+  .logo {
+    max-width: 277px;
+  }
+  nav {
+    margin-left: auto;
+  }
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 24px;
+  }
+  a {
+    color: #fff;
+  }
+  a:not(.btn):hover {
+    text-decoration: underline;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
   import "@fontsource/roboto-condensed"
   import "@fontsource/roboto-condensed/700.css"
   import "@fontsource/cabin"
+	import Header from '$lib/Header.svelte';
 </script>
 
+<Header/>
 <slot/>
