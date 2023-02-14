7b448ead9d6e9fa3ae36210a6455828bcd5e3a47
Andre Esteves Perrone <contato@artneo.com.br>
2023-02-14 08:36:00 -0300

    adicionar imagem do emoji

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   import imgHome from '../assets/musculacao.jpg'
+  import icon from '../assets/icon-muscle.png';
 </script>
 
 <section class="intro" style="background-image: url({imgHome})">
   <div class="intro__wrapper wrap">
-    <h1>Você está pronto<br> para o desafio?</h1>
+    <h1>Você está pronto<br> para o desafio? <img src="{icon}" alt=""></h1>
     <p>A Academia Local está pronta para te ajudar a chegar mais longe e alcançar sua melhor performance.</p>
     <a class="btn" href="/sobre">Sobre</a>
   </div>
@@ -36,5 +37,9 @@
   p {
     font-size: calc(18 / 16 * 1rem);
     margin-top: 16px;
+  }
+  img {
+    display: inline;
+    max-width: 36px;
   }
 </style>
