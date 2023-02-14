984da4fa4149ce7b33a322db0181a258450d9e18
Andre Esteves Perrone <contato@artneo.com.br>
2023-02-14 09:16:52 -0300

    criar um componente

---
+++ src/lib/Intro.svelte
@@ -0,0 +1,56 @@
+<script>
+  import imgMusculacao from '$lib/assets/musculacao.jpg'
+  import iconMuscle from '$lib/assets/icon-muscle.png';
+
+  export let title;
+  export let content;
+  export let btn;
+  export let href;
+  
+  export let img;
+  if (img === 'musculacao') img = imgMusculacao;
+  
+  export let icon;
+  if (icon === 'muscle') icon = iconMuscle
+</script>
+
+<section class="intro" style="background-image: url({img})">
+  <div class="intro__wrapper wrap">
+    <h1>{@html title} <img src="{icon}" alt=""></h1>
+    <p>{content}</p>
+    <a class="btn" href="{href}">{btn}</a>
+  </div>
+</section>
+
+<style>
+  .intro {
+    min-height: 360px;
+    background-size: cover;
+    background-position: center;
+    display: grid;
+    align-content: center;
+    justify-content: start;
+    position: relative;
+  }
+  .intro::after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background-color: rgba(0,0,0,0.55);
+  }
+  .intro__wrapper {
+    z-index: 1;
+    max-width: 450px;
+  }
+  .btn {
+    margin-top: 24px;
+  }
+  p {
+    font-size: calc(18 / 16 * 1rem);
+    margin-top: 16px;
+  }
+  img {
+    display: inline;
+    max-width: 36px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,12 @@
 <script>
-  import imgHome from '$lib/assets/musculacao.jpg'
-  import icon from '$lib/assets/icon-muscle.png';
+	import Intro from '$lib/Intro.svelte';
 </script>
 
-<section class="intro" style="background-image: url({imgHome})">
-  <div class="intro__wrapper wrap">
-    <h1>Você está pronto<br> para o desafio? <img src="{icon}" alt=""></h1>
-    <p>A Academia Local está pronta para te ajudar a chegar mais longe e alcançar sua melhor performance.</p>
-    <a class="btn" href="/sobre">Sobre</a>
-  </div>
-</section>
-
-<style>
-  .intro {
-    min-height: 360px;
-    background-size: cover;
-    background-position: center;
-    display: grid;
-    align-content: center;
-    justify-content: start;
-    position: relative;
-  }
-  .intro::after {
-    content: '';
-    position: absolute;
-    inset: 0;
-    background-color: rgba(0,0,0,0.55);
-  }
-  .intro__wrapper {
-    z-index: 1;
-    max-width: 450px;
-  }
-  .btn {
-    margin-top: 24px;
-  }
-  p {
-    font-size: calc(18 / 16 * 1rem);
-    margin-top: 16px;
-  }
-  img {
-    display: inline;
-    max-width: 36px;
-  }
-</style>
+<Intro
+  title="Você está pronto<br> para o desafio?"
+  content="A Academia Local está pronta para te ajudar a chegar mais longe e alcançar sua melhor performance."
+  btn="Sobre"
+  href="/"
+  img="musculacao"
+  icon="muscle"
+/>
