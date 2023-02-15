9f207d2b40284012975f681d8194d62d9796f9e1
Andre Esteves Perrone <contato@artneo.com.br>
2023-02-15 10:09:48 -0300

    animação: componente para perguntas e respostas

--- src/lib/Info.svelte
+++ src/lib/Info.svelte
@@ -1,5 +1,6 @@
 <script>
 	import IconPlus from "$lib/assets/IconPlus.svelte";
+  import autoAnimate from "@formkit/auto-animate"
 
   export let question;
   export let answer;
@@ -10,8 +11,8 @@
   }
 </script>
 
-<div class="info" role="button" on:click={handleActive} on:keydown={handleActive} tabindex="0">
-  <div class="question">
+<div class="info" role="button" on:click={handleActive} on:keydown={handleActive} tabindex="0" use:autoAnimate>
+  <div class="question" class:active={active}>
     <h3>{question}</h3>
     <IconPlus/>
   </div>
@@ -23,17 +24,30 @@
 </div>
 
 <style>
+  .info {
+    cursor: pointer;
+  }
   .question {
     display: flex;
     align-items: center;
     border: 1px solid var(--subtle);
     padding: 17px 16px;
     height: 80px;
+    transition: 250ms;
+  }
+  .question.active {
+    border-color: #fff;
+    transition: 125ms;
   }
   .question :global(svg) {
     margin-left: auto;
     margin-right: 8px;
     flex-shrink: 0;
+    transition: 250ms;
+  }
+  .question.active :global(svg) {
+    transform: rotate(45deg);
+    transition: 125ms;
   }
   .answer {
     padding: 0 16px;
