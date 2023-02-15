ba8685e5e5e3003e7f737bbf94db7d30f8de1209
Andre Esteves Perrone <contato@artneo.com.br>
2023-02-15 14:33:13 -0300

    treino: criar um componente para o single

---
+++ src/lib/IntroSingle.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let data;
+</script>
+
+<div class="intro">
+  <div class="intro__wrapper wrap">
+    <div class="intro__info">
+      <h1>{data.name}</h1>
+      <p>{data.content}</p>
+    </div>
+    <div class="intro__img">
+      <img src="{data.img}" alt="">
+    </div>
+  </div>
+</div>
+
+<style>
+  .intro__wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+    max-height: 360px;
+    align-items: center;
+  }
+  .intro__img {
+    margin-right: -20px;
+    position: relative;
+  }
+  .intro__img::before {
+    content: '';
+    display: block;
+    position: absolute;
+    inset: 0;
+    background-color: var(--overlay);
+    border-radius: 0 0 0 8px;
+  }
+  img {
+    border-radius: 0 0 0 8px;
+    height: 360px;
+    object-fit: cover;
+  }
+  p {
+    font-size: calc(18 / 16 * 1rem);
+    margin-top: 8px;
+  }
+</style>
--- src/routes/modalidades/[slug]/+page.svelte
+++ src/routes/modalidades/[slug]/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   import { page } from '$app/stores'
   import { modalidades } from '$lib/stores';
+	import IntroSingle from '$lib/IntroSingle.svelte';
   
   let modalidade = $page.params.slug;
   
   let data = $modalidades.find(e => e.slug === modalidade);
 </script>
 
-<h1>{data.name}</h1>
+<IntroSingle data={data} />
