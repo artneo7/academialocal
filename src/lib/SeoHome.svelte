3a6e42cd02ec75ae8166111c7cdc3de93cdc3b8f
Andre Esteves Perrone <contato@artneo.com.br>
2023-02-20 16:40:10 -0300

    open graph meta tags

--- src/lib/SeoHome.svelte
+++ src/lib/SeoHome.svelte
@@ -1,4 +1,5 @@
 <script>
+  import img from '$lib/assets/musculacao.avif';
   export let description;
   
   let siteName = 'Academia Local';
@@ -8,4 +9,9 @@
 <svelte:head>
   <title>{siteName}{separator}A academia pertinho de você!</title>
   <meta name="description" content="{description}">
+
+  <!-- Open Graph meta tags -->
+  <meta property="og:title" content="A academia pertinho de você!" />
+  <meta property="og:description" content="{description}" />
+  <meta property="og:image" content="{img}" />
 </svelte:head>
